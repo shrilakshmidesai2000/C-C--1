5289b7d52d66f43db395ca8768ff617c3493a1f2
VaibhavNitsure <52439634+VaibhavNitsure@users.noreply.github.com>
2019-11-23 00:48:41 +0530

    Update Shopping Cart.c

--- Shopping Cart/Shopping Cart.c
+++ Shopping Cart/Shopping Cart.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 #define mrow 10
 #define mcol 10
-int Add_item(int t[mrow][mcol],int e[mrow][mcol],int );
-int Delete_Item(int t[mrow][mcol],int e[mrow][mcol],int );
-int Display(int t[mrow][mcol],int v[mrow][mcol],int );
-int Bill_info(int t[mrow][mcol],int c[mrow][mcol],int );
+int Add_item(int t[mrow][mcol],int e[mrow][mcol],int ); //Add_item function declaration
+int Delete_Item(int t[mrow][mcol],int e[mrow][mcol],int ); //Delete_Item function declaration
+int Display(int t[mrow][mcol],int v[mrow][mcol],int ); //Display function declaration
+int Bill_info(int t[mrow][mcol],int c[mrow][mcol],int ); //Bill_info function declaration
 int main()
 {
     int i=0,j,k,n,s,z=0;
@@ -14,7 +14,7 @@ int main()
     printf("Enter the number of items in stock");
     scanf("%d",&n);
     int c1[mrow][mcol],c2[mrow][mcol]={0};
-    printf("Enter the elements in the stock\n");
+    printf("Enter the elements in the stock\n"); //Ask for stock elements
     for(s=0;s<n;s++)
     {
         printf("Item no.\tQuantity\tPrice\n");
@@ -30,7 +30,7 @@ int main()
     entry :
     printf("1 : Add an item\n2 : Delete an item\n3 : Display an item\n4 : Billing info\n");
     scanf("%d",&k);
-    switch(k)
+    switch(k) //switch for options
         {
             case 1 :
                 ptr=Add_item;
@@ -53,7 +53,7 @@ int main()
 
 }
 
-int Add_item(int c1[mrow][mcol],int c2[mrow][mcol],int l)
+int Add_item(int c1[mrow][mcol],int c2[mrow][mcol],int l) //Definition of Add_item
 {
     int a,b;
     void (*ptr2)(int c1[mrow][mcol],int c2[mrow][mcol],int n);
@@ -66,7 +66,7 @@ int Add_item(int c1[mrow][mcol],int c2[mrow][mcol],int l)
     ptr2=Display;
     (*ptr2)(c1,c2,l);
 }
-int Delete_Item(int c1[mrow][mcol],int c2[mrow][mcol],int l)
+int Delete_Item(int c1[mrow][mcol],int c2[mrow][mcol],int l) //Definition of Delete_item
 {
     int a,b;
     void (*ptr1)(int c1[mrow][mcol],int c2[mrow][mcol],int n);
@@ -80,7 +80,7 @@ int Delete_Item(int c1[mrow][mcol],int c2[mrow][mcol],int l)
     (*ptr1)(c1,c2,l);
 }
 
-int Display(int c1[mrow][mcol],int c2[mrow][mcol],int n)
+int Display(int c1[mrow][mcol],int c2[mrow][mcol],int n) //Definition of Display
 {
     int i,j;
     for(i=0;i<n;i++)
@@ -91,7 +91,7 @@ int Display(int c1[mrow][mcol],int c2[mrow][mcol],int n)
     }
 }
 
-int Bill_info(int c1[mrow][mcol],int c2[mrow][mcol],int n)
+int Bill_info(int c1[mrow][mcol],int c2[mrow][mcol],int n) //Definition of Bill_info
 {
     int r,w,sum=0;
     for(r=0;r<n;r++)
