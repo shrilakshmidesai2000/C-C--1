db862b222462fd97fdf937b56b60f6d7764c62af
VaibhavNitsure <52439634+VaibhavNitsure@users.noreply.github.com>
2019-11-23 00:44:21 +0530

    Update Inventory.c

--- Inventory Management/Inventory.c
+++ Inventory Management/Inventory.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-FILE *p;
+FILE *p; //pointer for FILE defined
 struct inventory{
     int asset_no;
     char name[10];
     float price;
-};
+}; //structure inventory defined 
 int main()
 {
-    struct inventory i;
+    struct inventory i; 
     char choice;
     int loop;
-    p=fopen("Inventory.txt","w");
+    p=fopen("Inventory.txt","w"); //File open
     if(p==NULL)
     {
         printf("FILE DOESNOT EXIST\n");
@@ -19,17 +19,17 @@ int main()
     {
         printf("Enter the asset number :");
         scanf("%d",&i.asset_no);
-        fprintf(p,"Asset Number : %d\n",i.asset_no);
+        fprintf(p,"Asset Number : %d\n",i.asset_no); //print in file
         printf("Enter the asset name :");
         getchar();
         scanf("%s",&i.name);
-        fprintf(p,"Asset Name : %s\n",i.name);
+        fprintf(p,"Asset Name : %s\n",i.name); //print in file
         printf("Enter the asset price :");
         scanf("%f",&i.price);
-        fprintf(p,"Asset Price : %.2f\n",i.price);
+        fprintf(p,"Asset Price : %.2f\n",i.price); //print in file
         printf("Do you want to add another asset :");
         getchar();
-        scanf("%c",&choice);
+        scanf("%c",&choice); //ask for choice
         if(choice=='y'||choice=='Y')
         {
             loop=1;
@@ -39,5 +39,5 @@ int main()
             loop=0;
         }
     }while(loop);
-    printf("*******Thank You********");
+    printf("*******Thank You********"); //end message
 }
