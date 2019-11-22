901da1dc7c0989a59dc2280e20b4825d0fe9468b
VaibhavNitsure <52439634+VaibhavNitsure@users.noreply.github.com>
2019-11-23 01:20:21 +0530

    CalculatorA

--- Simple Calculator/CalculatorA.c
+++ Simple Calculator/CalculatorA.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-float add(float a,float b)
+float add(float a,float b) //function definition of add()
 {
     return(a+b);
 }
-float sub(float a,float b)
+float sub(float a,float b //function definition of sub()
 {
     return(a-b);
 }
-float mul(float a,float b)
+float mul(float a,float b) //function definition of mul()
 {
     return(a*b);
-}
-float div(float a,float b)
+} 
+float div(float a,float b) //function definition of div()
 {
     return((float)a/b);
 }
-void display(char choice,float a,float b,float ans)
+void display(char choice,float a,float b,float ans) //function definition of display()
 {
     printf("%.3f %c %.3f is : %.2f",a,choice,b,ans);
 }
@@ -23,8 +23,8 @@ int main()
 {
     float a,b,ans;
     unsigned char choice;
-    float (*ptr)(float,float);
-    void (*ptr1)(char,float,float,float);
+    float (*ptr)(float,float); //pointer to access add(),sub(),mul(),div() function
+    void (*ptr1)(char,float,float,float); //pointer to access display()
     ptr1=display;
     printf("Enter the value of a and b :");
     scanf("%f%f",&a,&b);
@@ -32,7 +32,7 @@ int main()
     printf("Enter the operation to be performed on %.3f and %.3f :",a,b);
     getchar();
     choice = getchar();
-    switch(choice)
+    switch(choice) //Ask for choice
     {
         case'+' :
             ptr=add;
@@ -49,8 +49,8 @@ int main()
         default :
             printf("The choice entered is INCORRECT\nPlease enter the correct choice\n");
     }
-    ans = (*ptr)(a,b);
-    (*ptr1)(choice,a,b,ans);
+    ans = (*ptr)(a,b); //get return value in ans
+    (*ptr1)(choice,a,b,ans); 
     printf("\n");
-    goto pass;
+    goto pass; //loop from pass label
 }
